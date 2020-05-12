da01311d6a9d2a2f35be60d27d30f8e8a39b4d2a
EliorCohen <elior123456123123@gmail.com>
2020-05-12 20:47:45 +0300

    update

--- test/unit_tests/unit_test.dart
+++ test/unit_tests/unit_test.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:locationprojectflutter/data/model/models_location/result.dart';
 import 'package:locationprojectflutter/data/repository_impl/location_repo_impl.dart';
 
 void main() {
   group("Unit tests for app", () {
-    test('String should be reversed', () async {
+    test('String should be JSON', () async {
       LocationRepositoryImpl locationRepositoryImpl = LocationRepositoryImpl();
       double latitude = 31.7428444;
       double longitude = 34.9847567;
@@ -11,9 +12,10 @@ void main() {
       String type = 'bar';
       int valueRadiusText = 50000;
       String text = 'Bar';
-      String searchString = await locationRepositoryImpl.getLocationJson(
+      List<Result> searchString = await locationRepositoryImpl.getLocationJson(
           latitude, longitude, open, type, valueRadiusText, text);
-      expect(searchString, '');
+      expect(searchString, isNot(null));
+      expect(searchString, isNotEmpty);
     });
   });
 }
