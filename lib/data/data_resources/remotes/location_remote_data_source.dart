23476a4e10dba36ff22e56b3ff38de67ee01991e
EliorCohen <elior123456123123@gmail.com>
2020-05-27 03:20:29 +0300

    update

--- lib/data/data_resources/remotes/location_remote_data_source.dart
+++ lib/data/data_resources/remotes/location_remote_data_source.dart
@@ -4,6 +4,7 @@ import 'package:locationprojectflutter/core/constants/constant.dart';
 import 'package:locationprojectflutter/data/models/model_location/error.dart';
 import 'package:locationprojectflutter/data/models/model_location/place_response.dart';
 import 'package:locationprojectflutter/data/models/model_location/result.dart';
+//import 'package:dio/dio.dart';
 
 class LocationRemoteDataSource {
   static final LocationRemoteDataSource _singleton =
@@ -18,15 +19,19 @@ class LocationRemoteDataSource {
   String _baseUrl = Constants.baseUrl;
   String _API_KEY = Constants.API_KEY;
 
+//  Dio dio = new Dio();
+
   Future responseJsonLocation(double latitude, double longitude, String open,
       String type, int valueRadiusText, String text) async {
     String url =
         '$_baseUrl?key=$_API_KEY&location=$latitude,$longitude$open&types=$type&radius=$valueRadiusText&keyword=$text';
     print(url);
     final response = await http.get(url);
+//    final response = await dio.get(url); // dio
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       _handleResponse(data);
+//      _places = PlaceResponse.parseResults(response.data['results']); // dio
     } else {
       throw Exception('An error occurred getting places nearby');
     }
