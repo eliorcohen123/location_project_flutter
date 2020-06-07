56d7e77174a8b33b40117defc0e4e45f508ae613
EliorCohen <elior123456123123@gmail.com>
2020-06-07 16:09:58 +0300

    update

--- android/app/src/main/kotlin/com/mansh/purplle/location_project_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mansh.purplle.location_project_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/mansh/purplle/locationprojectflutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mansh.purplle.locationprojectflutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
