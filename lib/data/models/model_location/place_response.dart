ef5ab4cf572a066a582d1fd3ee90f0231a5fc1b7
EliorCohen <elior123456123123@gmail.com>
2020-06-09 18:39:20 +0300

    update

--- lib/data/models/model_location/place_response.dart
+++ lib/data/models/model_location/place_response.dart
@@ -1,7 +1,6 @@
 import 'package:locationprojectflutter/data/models/model_location/results.dart';
 
 class PlaceResponse {
-
   static List<Results> parseResults(List<dynamic> list) {
     return list
         .map(
